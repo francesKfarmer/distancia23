c20ccdb1b166cf4da533d4f9c8510df2f6f9d630
Marina Segura <108071165+francesKfarmer@users.noreply.github.com>
2023-05-31 09:23:04 +0200

    Upload script1-segura.php

---
+++ scripts/script1-segura.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Descripción del script:
+ * Este script contiene dos funciones para realizar operaciones de suma y resta.
+ * Autor/a:Marina Segura
+ * Versión: 1.0.0
+ *
+ * Etiquetas:
+ * @category Scripts
+ * @license  MIT
+ */
+
+/**
+ * Función para sumar dos números.
+ *
+ * @param float $num1 El primer número.
+ * @param float $num2 El segundo número.
+ * @return float El resultado de la suma.
+ *
+ * Etiquetas:
+ * @parametro
+ * @resultado
+ * @version 1.0.0
+ */
+function sumar($num1, $num2) {
+    $resultado = $num1 + $num2;
+    return $resultado;
+}
+
+/**
+ * Función para restar dos números.
+ *
+ * @param float $num1 El primer número.
+ * @param float $num2 El segundo número.
+ * @return float El resultado de la resta.
+ *
+ * Etiquetas:
+ * @parametro
+ * @resultado
+ * @version 1.0.0
+ */
+function restar($num1, $num2) {
+    $resultado = $num1 - $num2;
+    return $resultado;
+}
+
+// Ejemplo de uso de las funciones:
+$numero1 = 10;
+$numero2 = 5;
+
+$resultadoSuma = sumar($numero1, $numero2);
+echo "La suma de $numero1 y $numero2 es: $resultadoSuma<br>";
+
+$resultadoResta = restar($numero1, $numero2);
+echo "La resta de $numero1 y $numero2 es: $resultadoResta<br>";
+?>
